5e8f1130bcb0884572e5557e5fc403cae9505f85
Knowingest <harrisonmeyerhill@gmail.com>
2018-03-24 21:54:29 -0400

    reworking input to work from cstr.  not done shifting things over but close

--- main.c
+++ main.c
@@ -14,6 +14,11 @@ const unsigned int immask = 65535;         //immediate value mask
 const unsigned int shiftmask = 1984;      //shift amount mask
 const unsigned int fnmask = 63;          //function mask
 
+struct raw_data
+{
+    char input[32];
+};
+
 struct instruction_data
 {
     int address;
@@ -84,7 +89,7 @@ struct memwb
     int datafromalu;
 };
 
-struct dataMem
+struct memory_data
 {
     int address;
     int value;
@@ -97,49 +102,67 @@ struct branchpredictor
     int state;
 };
 
-void split_data(struct instruction_data* data);
-void print_data(int* regFile, struct dataMem* memory);
-int load_memory(struct instruction_data* data, struct dataMem* memory);
+int read_data(struct raw_data* src);
+void split_data(struct instruction_data* data, struct raw_data* src, int ending_index);
+void print_data(int* regFile, struct memory_data* memory);
+void load_memory(struct raw_data* src, struct memory_data* memory, int starting_index);
 void populate_branch_predictor(struct instruction_data* data);
 
 int main(void)
 {
     int i;
     int address = 0;
+    int data_index = 0;
     //create data arrays
-    struct instruction_data data_table[100];
-    struct branchpredictor branchpredict[100];
+    struct raw_data source_table[101];               //holds data in cstr form
+    struct instruction_data instruction_table[100];        //table of instructions as ints
+    struct branchpredictor branch_table[100];
    
-    int regFile[32];
-        for (i = 0; i < 32; ++i)
-            regFile[i] = 0;
-    struct dataMem memory[32];
+    int regFile[32];                //32 registers
+    for (i = 0; i < 32; ++i)
+        regFile[i] = 0;
+    struct memory_data memory[32];
 
-    //read data
-    for (i = 0; i < 100; ++i)       //scanf loop for reading data.
-    {                              //record entire instructions along with their address
-        if ((scanf("%d", &data_table[i].instruction)) != 1) break;  //break the scanf loop if we reach end of stream
-        data_table[i].address = address;
-        address += 4;
-    }
-    
-    for (; i < 100; ++i)                     //zero out the remaining array indices
-        data_table[i].instruction = 0;
-
-    split_data(data_table);    //split data into pieces
-    int dataStart = load_memory(data_table, memory);//load memory and save the starting address of the data section
-    //printf("start of data section: %d\n", dataStart);
+    data_index = read_data(source_table);       //read data and record start of data section
+    split_data(instruction_table, source_table, data_index - 1);    //split data into pieces
+    load_memory(source_table, memory, data_index);
     print_data(regFile, memory);    //print out data
 
     return 0;
 }
 
-//cuts out pieces of 32 bit instructions for future use
-void split_data(struct instruction_data* data)
+//reads data into array of cstr and returns start of data section
+int read_data(struct raw_data* src)
 {
-    int i;      //essentially we cut out individual portions of the instruction
-    for (i = 0; i < 100; ++i)
-    {   //we apply bit masks and shift the result forward so it can be easily read
+    int data_index;
+    int i;
+    for (i = 0; i < 101; ++i)
+    {
+        if (gets(src[i].input) == NULL) break; //read in data as array of cstr
+        //printf("%s\n", source_data[i].input);
+    }
+
+    for (; i < 101; ++i)                       //zero out unused section of array
+        src[i].input[0] = (char) 0;
+
+    for (i = 0; i < 101; ++i)                  //find where data section starts
+    {
+        if (src[i].input[0] == (char) 0) break;
+        ++data_index;
+    }
+    return data_index + 1;                     //return start of data section
+}
+
+//converts the source cstr array into an array of integers.
+//ends when it reaches the data section.
+//also cuts up the instruction so you can reference the individual components
+void split_data(struct instruction_data* data, struct raw_data* src, int ending_index)
+{
+    //printf("splitting data\n");
+    int i;
+    for (i = 0; i < ending_index; ++i)
+    {
+        sscanf(src[i].input, "%d", &data[i].instruction);
         data[i].opcode = (data[i].instruction & opmask) >> 26;
         data[i].rs = (data[i].instruction & rsmask) >> 21;
         data[i].rt = (data[i].instruction & rtmask) >> 16;
@@ -148,16 +171,19 @@ void split_data(struct instruction_data* data)
         data[i].shamt = (data[i].instruction & shiftmask) >> 6;
         data[i].func = (data[i].instruction & fnmask);
     }   //we can now reference each individual component of the instruction
-}     //(we store all of the components for all instructions, even though they won't all be used)
+    //(we store all of the components for all instructions, even though they won't all be used)
+    for (; i < 100; ++i)        //zero out remaining elements
+            data[i].instruction = 0;
+}     
 
 //print data for the current cycle
-void print_data(int* regFile, struct dataMem* memory)
+void print_data(int* regFile, struct memory_data* memory)
 {
     int i;
     printf("********************\nState at the beginning of cycle X:\n\tPC = X\n\tData Memory:\n");
 
     for (i = 0; i < 16; ++i)
-        printf("\t\tdataMem[%d] = %d\t\tdataMem[%d] = %d\n", i, memory[i].value, i + 16, memory[i + 16].value);
+        printf("\t\tmemory_data[%d] = %d\t\tmemory_data[%d] = %d\n", i, memory[i].value, i + 16, memory[i + 16].value);
 
     printf("\tRegisters:\n");
 
@@ -168,21 +194,19 @@ void print_data(int* regFile, struct dataMem* memory)
 }
 
 //return address of data section and populate data file
-int load_memory(struct instruction_data* data, struct dataMem* memory)
+void load_memory(struct raw_data* src, struct memory_data* memory, int starting_index)
 {
-    int i, j;
-    for (i = 0; i < 100; ++i)
-        if (data[i].instruction == 1)       //if we find the instruction 1, we know the data segment will follow
-        {
-            i += 1;
-            for (j = 0; j < 32 && i < 100; ++j)     //so we copy down the remaining "instructions" as data
-            {
-                memory[j].address = data[i].address;
-                memory[j].value = data[i++].instruction;    //we stop the loop after 32 iterations
-            }                                              //(default to zero if nothing is there to read)
-            break;
-        }          
-    return memory[0].address;   //return starting address of data section
+    //printf("loading memory\n");
+    int i, j, address;
+    j = 0;
+    address = (starting_index - 1) * 4; //starting line * 4 is the byte address
+    
+    for (i = starting_index; i < 101; ++i)  //(minus 1 because we have an empty line)
+    {
+        sscanf(src[i].input, "%d", &memory[j].value);
+        memory[j++].address = address;
+        address += 4;
+    }
 }
 
 void populate_branch_predictor(struct instruction_data* data)
